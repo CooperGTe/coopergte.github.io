a7ea25c92b821bbfb20fa5f99aa697518c9f3afc
CooperGTe <katsuro.gte@proton.me>
2024-09-01 14:54:46 +0700

    update somthing idk

--- util/navigator.js
+++ util/navigator.js
@@ -9,23 +9,43 @@ navstyle.setAttribute('rel', 'stylesheet');
 navstyle.setAttribute('href', '/util/navigator.css');
 navdiv.appendChild(navstyle);
 // HTML ELEMENT
-let head = document.createElement('h1');
-let navHead = document.createElement('p');
+let navHead = document.createElement('h3');
 
 headdiv.setAttribute('id', 'headdiv');
 navdiv.setAttribute('id', 'navigator');
-head.textContent = 'GT:ZE CS-G11';
-navHead.textContent = 'Pages Navigator';
+navHead.textContent = 'PAGE NAVIGATION';
 
 navdiv.appendChild(headdiv);
-navdiv.appendChild(head);
 navdiv.appendChild(navHead);
 navdiv.appendChild(document.createElement('br'));
 
+
+// TOP BAR
 let navhide = document.createElement('button');
 navhide.setAttribute('onclick', 'toggleNavbar()');
-document.body.appendChild(headdiv);
+
+let home = document.createElement('a');
+home.setAttribute('href', '/');
+home.setAttribute('id', 'home-button')
+home.textContent = 'HOME'
+
+let atherionfi = document.createElement('a');
+atherionfi.setAttribute('href', 'https://atherionfi.github.io')
+atherionfi.textContent = '| AtherionFi Sites'
+
+let peco = document.createElement('a');
+peco.setAttribute('href', 'https://raymond145.github.io')
+peco.textContent = '| Peco Sites'
+
+
 headdiv.appendChild(navhide);
+headdiv.appendChild(home);
+headdiv.appendChild(atherionfi);
+headdiv.appendChild(peco);
+document.body.appendChild(headdiv);
+
+
+
 
 const width = window.innerWidth;
 if (width > 768) {
